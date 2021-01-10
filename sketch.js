d6a0dfec8b1068f579a866718a457ec9cdbde49e
Sarvesh <drvsraja2000@gmail.com>
2021-01-10 16:55:51 +0530

    smoke image

--- sketch.js
+++ sketch.js
@@ -8,6 +8,8 @@ var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
 
+var gamestate="sling"
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -69,16 +71,19 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if(gamestate==="sling"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gamestate="launched"
 }
 
 function keyPressed(){
     if(keyCode === 32){
-        slingshot.attach(bird.body);
+       // slingshot.attach(bird.body);
     }
 }
---
+++ sketch2.js
@@ -0,0 +1,47 @@
+// Data types
+
+var numberExample = 5;
+var stringExample = "hello"
+var booleanExample = true;
+var nothing = null;
+
+var test; // var declaration only
+
+console.log(numberExample);
+console.log(stringExample);
+console.log(booleanExample);
+console.log(nothing);
+console.log(test);
+
+// arrays
+
+var testArray = []; // empty array
+
+// array index always start at 0 and always end at the (length of array - 1)
+
+var arrayExample = [1,2,3,4,5]
+
+console.log(arrayExample);
+
+console.log(arrayExample[2]);
+
+console.log(arrayExample.length);
+
+
+var student1 = ["Sarvesh", 13]
+
+var student2 = ["Saranya", 15]
+
+var student3 = ["Dan", 14]
+
+var allStudents = [student1, student2, student3];
+
+console.log(allStudents)
+console.log(allStudents[0]);
+
+console.log(allStudents[allStudents.length - 1]);
+
+console.log(allStudents[0][0]);
+
+
+
